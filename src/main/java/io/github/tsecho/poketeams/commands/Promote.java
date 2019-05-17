743246d3b1c5f810257599be766a46dc9687e5b9
TSEcho <21craftn@gmail.com>
2019-05-17 01:44:58 -0400

    fixed promotion issue

--- src/main/java/io/github/tsecho/poketeams/commands/Promote.java
+++ src/main/java/io/github/tsecho/poketeams/commands/Promote.java
@@ -39,8 +39,9 @@ public class Promote implements CommandExecutor {
 			return ErrorCheck.test(src, ErrorMessages.INSUFFICIENT_RANK);
 		if(roleOther.getPlace() + 1 >= 5)
 			return ErrorCheck.test(src, ErrorMessages.CANT_PROMOTE);
-		if((role.getPlace() <= (roleOther.getPlace() + 1)) || role.getPlace() != 4)
-			return ErrorCheck.test(src, ErrorMessages.INSUFFICIENT_RANK);
+		if(role.getPlace() <= (roleOther.getPlace() + 1))
+			if(role.getPlace() != 4)
+				return ErrorCheck.test(src, ErrorMessages.INSUFFICIENT_RANK);
 
 		if(roleOther.getPlace() == 3 && role.getPlace() == 4) {
 
